4fa5aeaabfd416533f9c98e9637f26bab32692ad
Nil <nilbasora@gmail.com>
2023-04-08 00:37:18 +0200

    t5

--- components/getPostMetadata.ts
+++ components/getPostMetadata.ts
@@ -3,22 +3,19 @@ import matter from "gray-matter";
 import { PostMetadata } from "../components/PostMetadata";
 
 const getPostMetadata = (subfolder: string): PostMetadata[] => {
-  const path = require("path");
-  const folder = path.join(process.cwd(), `posts/${subfolder}/`);
+  const folder = `posts/${subfolder}/`;
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
   // Get gray-matter data from each file.
   const posts = markdownPosts.map((fileName) => {
-    const path = require("path");
-    const file = path.join(process.cwd(), `posts/${subfolder}/${fileName}`);
-    const fileContents = fs.readFileSync(file, "utf8");
+    const fileContents = fs.readFileSync(`posts/${subfolder}/${fileName}`, "utf8");
     const matterResult = matter(fileContents);
     return {
       title: matterResult.data.title,
       date: matterResult.data.date,
       subtitle: matterResult.data.subtitle,
-      slug: `${subfolder}/${fileName.replace(".md", "")}`,
+      slug: `${fileName.replace(".md", "")}`,
     };
   });
 
