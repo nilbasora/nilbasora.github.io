e188f1074162d0458fb996bb4fa29bd3cf79aec5
Nil <nilbasora@gmail.com>
2023-04-08 00:01:54 +0200

    trying to fix github deploy error

--- components/getPostMetadata.ts
+++ components/getPostMetadata.ts
@@ -3,13 +3,13 @@ import matter from "gray-matter";
 import { PostMetadata } from "../components/PostMetadata";
 
 const getPostMetadata = (subfolder: string): PostMetadata[] => {
-  const folder = `posts/${subfolder}/`;
+  const folder = `/posts/${subfolder}/`;
   const files = fs.readdirSync(folder);
   const markdownPosts = files.filter((file) => file.endsWith(".md"));
 
   // Get gray-matter data from each file.
   const posts = markdownPosts.map((fileName) => {
-    const fileContents = fs.readFileSync(`posts/${subfolder}/${fileName}`, "utf8");
+    const fileContents = fs.readFileSync(`/posts/${subfolder}/${fileName}`, "utf8");
     const matterResult = matter(fileContents);
     return {
       title: matterResult.data.title,
